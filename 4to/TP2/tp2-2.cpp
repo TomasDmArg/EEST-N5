6b958ce6aad2f4ed01a73aa987bba4f390f28cd1
Tomas <tomasdimauro1@gmail.com>
2021-03-19 17:02:14 -0300

    Se añadió el tp2

---
+++ 4to/TP2/tp2-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int i=4, x=5;
+	for(i=0; i<10; i++){
+		if(i<x){
+			cout <<i;
+		}else{
+			cout <<i-x;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+- En esta rama se desarrollan los trabajos antes de publicarlos en la rama principal
