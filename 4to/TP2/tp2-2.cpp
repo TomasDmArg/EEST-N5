577cfba54a50417947e42d1d924dcec45083f125
Tomas <tomasdimauro1@gmail.com>
2021-03-19 17:18:48 -0300

    Solucion de errores

--- 4to/TP2/tp2-2.cpp
+++ 4to/TP2/tp2-2.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
-int main (){
-	int i=4, x=5;
-	for(i=0; i<10; i++){
-		if(i<x){
-			cout <<i;
-		}else{
-			cout <<i-x;
-		}
-	}
+int main(){
+	float ars;
+	    string separador = " ------------------";
+	    cout << "\n Pesos argentinos (ARS): \n > ";
+	    cin >> ars;
+	    cout << separador << "\n";
+		cout << "\n USD:" << ars/140 << "\n EUR:" << ars/165.28 << "\n BRL:" << ars/16.65 <<  "\n UYU:" << ars/2.06;
+    return 0;
 }
