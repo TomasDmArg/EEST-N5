acfe6ffbcdd76594f03bebca50cff09f09f23d09
Tomas Di Mauro <tomasdimauro1@gmail.com>
2021-03-19 17:12:38 -0300

    Update tp2.cpp

--- 4to/TP2/tp2.cpp
+++ 4to/TP2/tp2.cpp
@@ -2,74 +2,71 @@
 using namespace std;
 
 int start(){
-		int type, typec;
-	    float a;
-	    string separador = " ------------------";
-		cout << "\n Elige la medida: \n --> 1. Yardas \n --> 2. Pulgadas \n --> 3. Centimetros \n --> 4. Metros \n ------------------ \n  > " ;
+	int type, typec;
+	float a;
+	string separador = " ------------------";
+	cout << "\n Elige la medida: \n --> 1. Yardas \n --> 2. Pulgadas \n --> 3. Centimetros \n --> 4. Metros \n ------------------ \n  > " ;
     	cin >> type;
     	if(type > 4){
     	cout << "La medida seleccionada es incorrecta";	
     	cout << "\n" << separador << "\n";
     	start();
-		}else{
-			cout << separador;
+	}else{
+		cout << separador;
     		cout << "\n Valor numerico de la medida: \n";	
     		cout << separador << "\n > ";
-			cin >> a;
-    		
+		cin >> a;
     		cout << separador;
     		cout << "\n Elige la medida a la cual queres convertir: \n --> 1. Yardas \n --> 2. Pulgadas \n --> 3. Centimetros \n --> 4. Metros \n ------------------ \n > " ;
 	    	cin >> typec;
 	    	cout << separador << "\n";
-			if(typec > 4){
-	    		cout << " La medida seleccionada es incorrecta";	
-	    		cout << "\n" << separador << "\n";
-	    		start();
+		if(typec > 4){
+			cout << " La medida seleccionada es incorrecta";	
+			cout << "\n" << separador << "\n";
+			start();
+		}else{
+			if(type == typec){
+				cout << " Las medidas son iguales" << endl;
+				cout << separador << "\n";
+				start();
 			}else{
-				if(type == typec){
-					cout << " Las medidas son iguales" << endl;
-					cout << separador << "\n";
-					start();
-				}else{
-					switch(type){
-						case 1 : 
-							a = a*91.44;
-							break;
-						case 2 : 
-							a = a*2.54;
-							break;
-						case 3 : 
-							a = a+0;
-							break;
-						case 4 : 
-							a = a*100;
-							break;
-					}
-					switch(typec){
-						case 1 :
-							a = a/91.44;
-							cout << " El resultado es: \n > " << a << "yd";
-							break;
-						case 2 : 
-							a = a/2.54;
-							cout << " El resultado es: \n > " << a << "in";
-							break;
-						case 3 : 
-							a = a+0;
-							cout << " El resultado es: \n > " << a << "cm";
-							break;
-						case 4 : 
-							a = a/100;
-							cout << " El resultado es: \n > " << a << "m";
-							break;
-					}
+				switch(type){
+					case 1 : 
+						a = a*91.44;
+						break;
+					case 2 : 
+						a = a*2.54;
+						break;
+					case 3 : 
+						a = a+0;
+						break;
+					case 4 : 
+						a = a*100;
+						break;
+				}
+				switch(typec){
+					case 1 :
+						a = a/91.44;
+						cout << " El resultado es: \n > " << a << "yd";
+						break;
+					case 2 : 
+						a = a/2.54;
+						cout << " El resultado es: \n > " << a << "in";
+						break;
+					case 3 : 
+						a = a+0;
+						cout << " El resultado es: \n > " << a << "cm";
+						break;
+					case 4 : 
+						a = a/100;
+						cout << " El resultado es: \n > " << a << "m";
+						break;
 				}
 			}
 		}
-		
 	}
+}
 int main(){
-	start();
-    
-    return 0;
+	start();	
+	return 0;
 }
