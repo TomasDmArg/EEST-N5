d17661f9de4e57be44262fa7dd7927e3bc0021f0
Tomas <tomasdimauro1@gmail.com>
2021-04-05 18:21:17 -0300

    Se añadió OIAJ II

--- 4to/OIAJ/orderWords(4).cpp
+++ 4to/OIAJ/orderWords(4).cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
-  
-int main(){
-	string text1;
-	std::vector<int> modText1 = {1,1,1,1};
-	//int modText1[] = {1,1,1,1};
-	string letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-    //char letters[27][1] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	cin>>text1;
-	//cout<<letters.find(text1[i], 0;
-	//modText1.resize(text1.size());
-	for(int i = 0; i < text1.size(); i++){
-		char findThem = text1[i];
-		cout<<findThem<<endl;
-		cout<<letters.find(findThem)<<endl;
-		modText1.push_back(letters.find(findThem));
-		//modText1[i] = letters.find(findThem);
-		cout<<modText1<<endl;
+int main () {
+	string a, b;
+	cin>>a>>b;
+	if(a < b){
+		cout<<"LA PRIMERA";
+	}else if( a > b){
+		cout<<"LA SEGUNDA";
+	}else if( a == b){
+		cout<<"SON IGUALES";
 	}
-	cout<<modText1;
 	return 0;
 }
