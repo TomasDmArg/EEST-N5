a6d133b7b225d995f824d2ca261a9e4e0ecf79f8
Tomas <tomasdimauro1@gmail.com>
2021-06-02 19:53:22 -0300

    Upd

--- 4to/tp28.cpp
+++ 4to/tp28.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-int esMaximo(int a, int b, int c), num1, num2, num3, x;
+int esMaximo(int a, int b, int c), num1, num2, num3;
 int main (){
     cout<<"Ingrese tres numeros";cin>>num1>>num2>>num3;
     cout<<esMaximo(num1,num2,num3);
     return 0;
 }
 esMaximo(int a, int b, int c){
-    x = a;
+    int x = a;
     if(b > x) x = b;
     if(c > x) x = c;
     return x;
