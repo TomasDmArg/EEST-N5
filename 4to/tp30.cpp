65b5694dda2fb8e686c57267b903ae8eb2147b21
Tomas <tomasdimauro1@gmail.com>
2021-06-02 20:25:25 -0300

    Upd

--- 4to/tp30.cpp
+++ 4to/tp30.cpp
@@ -4,6 +4,6 @@ int doble(int x){return x*2;};
 int main (){
 	int num1;
     cout<<"Ingrese un numero: "; cin>>num1;
-    cout<<doble(num1)
+    cout<<"El doble es: "<<doble(num1);
     return 0;
 }
