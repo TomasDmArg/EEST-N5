033672d30f3e1c6b426894f9963573bf6e41ba54
Tomas <tomasdimauro1@gmail.com>
2021-06-02 19:53:59 -0300

    Upd

--- 4to/tp28.cpp
+++ 4to/tp28.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int esMaximo(int a, int b, int c), num1, num2, num3;
 int main (){
-    cout<<"Ingrese tres numeros";cin>>num1>>num2>>num3;
-    cout<<esMaximo(num1,num2,num3);
+    cout<<"Ingrese tres numeros: ";
+    cin>>num1>>num2>>num3;
+    cout<<"El maximo es: "<<esMaximo(num1,num2,num3);
     return 0;
 }
 esMaximo(int a, int b, int c){
